d376892f041191af060369a4ff290f2396e064d2
Carlos Quesada <carlos.quesada@newelevation.com>
2019-11-03 01:34:07 -0400

    Program for a sequential/fallback checkout

--- src/config.js
+++ src/config.js
@@ -15,3 +15,61 @@ export const initial_sequence = [
   "order_confirmation",
   "end_interaction"
 ];
+/*
+# Linguistic setup
+const { fallback as either, sequence } = parseq;
+
+# Program
+sequence([
+  x('Review cart'),
+  x('Apply promo code'),
+  x('Select account mode'),
+  x('Select shipping address'),
+  x('Select shipping method'),
+  x('Select payment method'),
+  x('Verify order'),
+])(x('Order confirmation'));
+
+const interactions = {
+  'Select account mode': either([
+    x('Sign in or create account'),
+    x('Proceed as guest') // + retry sign-in button (?)
+  ]),
+  'Sign in or create account': sequence([
+    x('Enter username'), // checks if the username exists in the DB
+    x('Submit password') // sign in or create an account // reply we sent you email confirmation
+  ]),
+  'Select shipping address': sequence([
+    x('Enter first and last name'),
+    x('Enter shipping address'),
+    x('Enter phone number'),
+    x('Enter email address'),
+    x('Sign up for newsletter'), // submits form to backend
+    either([
+      x('Verify shipping'),
+      x('Select shipping address')
+    ])
+  ]),
+  'Select shipping method',  // submits form to backend
+  'Select payment method': either([
+    x('Pay with PayPal'),
+    x('Pay with a credit card')
+  ]),
+  'Pay with a credit card': sequence([
+    x('Enter name on card'),
+    x('Enter credit card number'),
+    x('Enter expiration MM/YY'),
+    x('Enter security code (CVV)'),
+    either([
+      x('Use shipping address as billing address')
+      x('Enter billing address')
+    ])
+  ]),
+  'Verify order', // e.g.: click "Edit shipping method" adds an x('Select shipping method') before this one.
+  'Order confirmation'
+};
+function x (s) {
+  if (typeof interactions[s] === 'function') return interactions[s]
+  if (components[s]) return components[s]
+  throw new Error(`Please create interaction "${s}"`)
+}*/
